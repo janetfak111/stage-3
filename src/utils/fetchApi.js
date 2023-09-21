046b3121910b7c38748937b26b16e50f908bab78
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-09-21 16:16:19 +0000

    final commit

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getRandomImages = async () => {
   try {
     const { data } = await axios.get(
-      "https://api.unsplash.com/search/photos/?client_id=-TKWnf0lgg5v-IuEg3Cbub1RLev6DP26LWbIzfD97F0&query=girls"
+      "https://api.unsplash.com/search/photos/?client_id=-TKWnf0lgg5v-IuEg3Cbub1RLev6DP26LWbIzfD97F0&query=forest"
     );
     return data;
   } catch (error) {
